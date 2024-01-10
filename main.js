7a3362bff0fa6b6de6a69ecc2fe9981904a90477
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-10 13:15:11 +0530

    got values entered by user

---
+++ main.js
@@ -0,0 +1,5 @@
+const date=document.querySelector('#date');
+const category=document.querySelector('#category');
+const amount=document.querySelector('#amount');
+
+console.log(date + category + amount);
